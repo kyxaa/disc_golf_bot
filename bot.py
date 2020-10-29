f8cc458556763775d8301e7e8fc74f1ea9c1972c
Lucas <lucaswfreeman@gmail.com>
2020-10-29 15:07:53 -0500

    added HourlyUpdate cog and implemented channel scan

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import discord
-from discord.ext import commands
+from discord.ext import tasks, commands
 from config import INVOCATION, DATETIME_STRING_FORMAT, GITHUB_REPO, SUNNY, FEW_CLOUDS, SCATTERED_CLOUDS, BROKEN_CLOUDS, SHOWER_RAIN, RAIN, THUNDERSTORM, SNOW, MIST
 from datetime import datetime
 import requests
@@ -7,6 +7,7 @@ import os
 import json
 from dotenv import load_dotenv
 import asyncio
+import asyncpg
 bot = commands.Bot(command_prefix=INVOCATION)
 
 load_dotenv()
@@ -29,9 +30,10 @@ async def on_ready():
     await bot.change_presence(activity=git_status)
 
 
-class MyCog(commands.Cog):
+class HourlyUpdate(commands.Cog):
     def __init__(self):
         self.index = 0
+        self.bot = bot
         self.printer.start()
 
     def cog_unload(self):
@@ -39,8 +41,20 @@ class MyCog(commands.Cog):
 
     @tasks.loop(seconds=5.0)
     async def printer(self):
-        print(self.index)
-        self.index += 1
+        await self.scan_channel()
+
+    @printer.before_loop
+    async def before_printer(self):
+        print('waiting...')
+        await self.bot.wait_until_ready()
+
+    async def scan_channel(self):
+        channels = bot.get_all_channels()
+        for channel in channels:
+            if channel.name == "course-info":
+                pinned_messages = await channel.pins()
+                for messages in pinned_messages:
+                    pass
 
 
 #######################################################################
@@ -60,9 +74,8 @@ def weather_lat_long(lat, lon):
         json.dump(data, outfile)
     pass
 
-
 # weather_lat_long(33.044564, -96.691857)
 
 
-bot.loop.create_task(weather_update())
+bot.add_cog(HourlyUpdate())
 bot.run(DISCORD_TOKEN)
