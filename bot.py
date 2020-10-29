0fd3ceab397c304443e201f29fecbf4c6c70a739
Lucas <lucaswfreeman@gmail.com>
2020-10-29 09:33:34 -0500

    Adding some TODOs, organizing code, and copying code

--- bot.py
+++ bot.py
@@ -6,11 +6,20 @@ import requests
 import os
 import json
 from dotenv import load_dotenv
+import asyncio
 bot = commands.Bot(command_prefix=INVOCATION)
 
 load_dotenv()
-weather_token = os.getenv('WEATHER_TOKEN')
+WEATHER_TOKEN = os.getenv('WEATHER_TOKEN')
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 
+
+#######################################################################
+# Discord Stuff
+# TODO: Create an update system that has properly handling...lets not make this another Calendra fiasco
+# TODO: Create information scrapping from messages in specific channel.
+# TODO: I would like the bot to create a "global" variable that is the channel that we are updating course info. I would like to this to be based on channel name.
+#######################################################################
 
 @bot.event
 async def on_ready():
@@ -19,9 +28,29 @@ async def on_ready():
     await bot.change_presence(activity=git_status)
 
 
+class MyCog(commands.Cog):
+    def __init__(self):
+        self.index = 0
+        self.printer.start()
+
+    def cog_unload(self):
+        self.printer.cancel()
+
+    @tasks.loop(seconds=5.0)
+    async def printer(self):
+        print(self.index)
+        self.index += 1
+
+
+#######################################################################
+# Weather Stuff
+# TODO: Create "data presentation" functionality.
+# TODO: Create ways to force an update
+#######################################################################
+
 def weather_lat_long(lat, lon):
     r = requests.get(
-        f'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid={weather_token}&units=imperial')
+        f'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid={WEATHER_TOKEN}&units=imperial')
     if not r.ok:
         # return
         raise Exception("ERROR: Could not fetch weather")
@@ -31,4 +60,8 @@ def weather_lat_long(lat, lon):
     pass
 
 
-weather_lat_long(33.044564, -96.691857)
+# weather_lat_long(33.044564, -96.691857)
+
+
+bot.loop.create_task(weather_update())
+bot.run(DISCORD_TOKEN)
