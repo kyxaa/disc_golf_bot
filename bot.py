f59ad9d14401e0b82b3fc76bc0cccf64e6916d3f
Lucas <lucaswfreeman@gmail.com>
2020-10-30 08:46:32 -0500

    bot is able to add embed to messages from json

--- bot.py
+++ bot.py
@@ -16,14 +16,6 @@ WEATHER_TOKEN = os.getenv('WEATHER_TOKEN')
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 
 
-#######################################################################
-# Discord Stuff
-# TODO: Create an update system that has properly handling...lets not make this another Calendra fiasco
-# TODO: Create information scrapping from messages in specific channel.
-# TODO: I would like the bot to create a "global" variable that is the channel that we are updating course info. I would like to this to be based on channel name.
-# TODO: Test Github webhooks ROUND 4
-#######################################################################
-
 @bot.event
 async def on_ready():
     print("Connected!")
@@ -31,7 +23,7 @@ async def on_ready():
     await bot.change_presence(activity=git_status)
 
 
-class IntervalUpdate(commands.Cog):
+class WeatherUpdate(commands.Cog):
     def __init__(self):
         self.index = 0
         self.bot = bot
@@ -42,14 +34,14 @@ class IntervalUpdate(commands.Cog):
 
     @tasks.loop(seconds=60.0)
     async def update(self):
-        await self.scan_channel()
+        await self.scan_channel_and_update_embeds()
 
     @update.before_loop
     async def before_update(self):
         print('waiting...')
         await self.bot.wait_until_ready()
 
-    async def scan_channel(self):
+    async def scan_channel_and_update_embeds(self):
         channels = bot.get_all_channels()
         for channel in channels:
             if channel.name == "course-info":
@@ -58,30 +50,41 @@ class IntervalUpdate(commands.Cog):
                     coordinates_match = re.search(
                         "\(([0-9.,\s-]*)\)", message.content)
                     coordinates_list = coordinates_match.group(1).split(", ")
-                    weather_info = weather_lat_long(
+                    weather_info = self.weather_lat_long(
                         coordinates_list[0], coordinates_list[1])
+                    await self.update_embed(message, weather_info)
                     pass
 
+    async def update_embed(self, message, weather_info):
+        with open('embed_json.json', 'r') as infile:
+            data = json.load(infile)
+        embed = discord.Embed.from_dict(data)
+        await message.edit(embed=embed)
 
-#######################################################################
-# Weather Stuff
-# TODO: Create "data presentation" functionality.
-# TODO: Create ways to force an update
-#######################################################################
+    def weather_lat_long(self, lat, lon):
+        # r = requests.get(
+        #     f'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid={WEATHER_TOKEN}&units=imperial')
+        # if not r.ok:
+        #     # return
+        #     raise Exception("ERROR: Could not fetch weather")
+        # data = r.json()
+        # with open('course_data.json', 'w') as outfile:
+        #     json.dump(data, outfile)
+        ##########Only here during testing##########
+        ############################################
+        with open('course_data.json', 'r') as infile:
+            data = json.load(infile)
+        ############################################
+        return data
 
-def weather_lat_long(lat, lon):
-    r = requests.get(
-        f'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid={WEATHER_TOKEN}&units=imperial')
-    if not r.ok:
-        # return
-        raise Exception("ERROR: Could not fetch weather")
-    data = r.json()
-    with open('course_data.json', 'w') as outfile:
-        json.dump(data, outfile)
-    return data
 
 # weather_lat_long(33.044564, -96.691857)
-
-
-bot.add_cog(IntervalUpdate())
+bot.add_cog(WeatherUpdate())
 bot.run(DISCORD_TOKEN)
+
+#########Add Park Template#########
+# await channel.send(content="**SHAWNEE PARK** (33.044564, -96.691857)\n\
+# ===============================\n\
+# Google Maps Link: https://goo.gl/maps/n7ThZ1CQeKvJvoDH6\n\
+# \n\
+# UDisc Link: https://app.udisc.com/applink/course/2307")
