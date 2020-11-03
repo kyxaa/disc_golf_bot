a404ba27cdf8322630392f56f6630bf8fe6a64a6
Lucas <lucaswfreeman@gmail.com>
2020-11-03 08:08:27 -0600

    stored direction data as tuple list

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import tasks, commands
-from config import INVOCATION, DATETIME_STRING_FORMAT, GITHUB_REPO, SUNNY, FEW_CLOUDS, SCATTERED_CLOUDS, BROKEN_CLOUDS, SHOWER_RAIN, RAIN, THUNDERSTORM, SNOW, FOG, ICON_CODE_TUPLE_LIST
+from config import INVOCATION, DATETIME_STRING_FORMAT, GITHUB_REPO, SUNNY, FEW_CLOUDS, SCATTERED_CLOUDS, BROKEN_CLOUDS, SHOWER_RAIN, RAIN, THUNDERSTORM, SNOW, FOG, ICON_CODE_TUPLE_LIST, DIRECTION_DEGREES_TUPLE_LIST
 from datetime import datetime
 import requests
 import os
@@ -43,45 +43,50 @@ class WeatherUpdate(commands.Cog):
         print('waiting...')
         await self.bot.wait_until_ready()
 
-    def directions_from_degrees(self, degrees_direction):
-        if (348.75 <= degrees_direction < 360) or (0 <= degrees_direction < 11.25):
-            wind_direction = "N"
-        elif (11.25 <= degrees_direction < 33.75):
-            wind_direction = "NNE"
-        elif (33.75 <= degrees_direction < 56.25):
-            wind_direction = "NE"
-        elif (56.25 <= degrees_direction < 78.75):
-            wind_direction = "ENE"
-        elif (78.75 <= degrees_direction < 101.25):
-            wind_direction = "E"
-        elif (101.25 <= degrees_direction < 123.75):
-            wind_direction = "ESE"
-        elif (123.75 <= degrees_direction < 146.25):
-            wind_direction = "SE"
-        elif (146.25 <= degrees_direction < 168.75):
-            wind_direction = "SSE"
-        elif (168.75 <= degrees_direction < 191.25):
-            wind_direction = "S"
-        elif (191.25 <= degrees_direction < 213.75):
-            wind_direction = "SSW"
-        elif (213.75 <= degrees_direction < 236.25):
-            wind_direction = "SW"
-        elif (236.25 <= degrees_direction < 258.75):
-            wind_direction = "WSW"
-        elif (258.75 <= degrees_direction < 281.25):
-            wind_direction = "W"
-        elif (281.25 <= degrees_direction < 303.75):
-            wind_direction = "WNW"
-        elif (303.75 <= degrees_direction < 326.25):
-            wind_direction = "NW"
-        elif (326.25 <= degrees_direction < 348.75):
-            wind_direction = "NNW"
-        return wind_direction
+    # def directions_from_degrees(self, degrees_direction):
+    #     if (348.75 <= degrees_direction < 360) or (0 <= degrees_direction < 11.25):
+    #         wind_direction = "N"
+    #     elif (11.25 <= degrees_direction < 33.75):
+    #         wind_direction = "NNE"
+    #     elif (33.75 <= degrees_direction < 56.25):
+    #         wind_direction = "NE"
+    #     elif (56.25 <= degrees_direction < 78.75):
+    #         wind_direction = "ENE"
+    #     elif (78.75 <= degrees_direction < 101.25):
+    #         wind_direction = "E"
+    #     elif (101.25 <= degrees_direction < 123.75):
+    #         wind_direction = "ESE"
+    #     elif (123.75 <= degrees_direction < 146.25):
+    #         wind_direction = "SE"
+    #     elif (146.25 <= degrees_direction < 168.75):
+    #         wind_direction = "SSE"
+    #     elif (168.75 <= degrees_direction < 191.25):
+    #         wind_direction = "S"
+    #     elif (191.25 <= degrees_direction < 213.75):
+    #         wind_direction = "SSW"
+    #     elif (213.75 <= degrees_direction < 236.25):
+    #         wind_direction = "SW"
+    #     elif (236.25 <= degrees_direction < 258.75):
+    #         wind_direction = "WSW"
+    #     elif (258.75 <= degrees_direction < 281.25):
+    #         wind_direction = "W"
+    #     elif (281.25 <= degrees_direction < 303.75):
+    #         wind_direction = "WNW"
+    #     elif (303.75 <= degrees_direction < 326.25):
+    #         wind_direction = "NW"
+    #     elif (326.25 <= degrees_direction < 348.75):
+    #         wind_direction = "NNW"
+    #     return wind_direction
+
+    def fetch_direction_with_degrees(self, degrees):
+        for item in DIRECTION_DEGREES_TUPLE_LIST:
+            if item[0] <= degrees < item[1]:
+                return item[2]
 
     def fetch_emoji_with_icon_code(self, icon_code):
-        for pair in ICON_CODE_TUPLE_LIST:
-            if pair[0] == icon_code:
-                return pair[1]
+        for item in ICON_CODE_TUPLE_LIST:
+            if item[0] == icon_code:
+                return item[1]
 
     async def scan_channel_and_update_embeds(self):
         channels = bot.get_all_channels()
@@ -113,7 +118,7 @@ class WeatherUpdate(commands.Cog):
         current_weather_icon = self.fetch_emoji_with_icon_code(
             weather_info["current"]["weather"][0]["icon"])
         pass
-        current_wind_direction = self.directions_from_degrees(
+        current_wind_direction = self.fetch_direction_with_degrees(
             weather_info["current"]["wind_deg"])
         current_datetime = datetime.fromtimestamp(
             weather_info["current"]["dt"], tz=timezone(weather_info["timezone"]))
--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 INVOCATION = "!"
 DATETIME_STRING_FORMAT = "%m/%d/%y %H:%M"
 GITHUB_REPO = "https://github.com/kyxaa/disc_golf_bot"
+
+# emojis for weather
 SUNNY = ":sunny:"
 FEW_CLOUDS = ":white_sun_snall_cloud:"
 SCATTERED_CLOUDS = ":cloud:"
@@ -10,6 +12,8 @@ RAIN = ":cloud_rain:"
 THUNDERSTORM = ":thunder_cloud_rain:"
 SNOW = ":cloud_snow:"
 FOG = ":fog:"
+
+# list for converting the Open Weather codes into the above emojis
 ICON_CODE_TUPLE_LIST = [
     ("01d", SUNNY),
     ("02d", FEW_CLOUDS),
@@ -20,3 +24,24 @@ ICON_CODE_TUPLE_LIST = [
     ("11d", THUNDERSTORM),
     ("13d", SNOW),
     ("50n", FOG)]
+
+# list for converting degrees into directions
+DIRECTION_DEGREES_TUPLE_LIST = [
+    (348.75, 361, "N"),
+    (0, 11.25, "N"),
+    (11.25, 33.75, "NNE"),
+    (33.75, 56.25, "NE"),
+    (56.25, 78.75, "ENE"),
+    (78.75, 101.25, "E"),
+    (101.25, 123.75, "ESE"),
+    (123.75, 146.25, "SE"),
+    (146.25, 168.75, "SSE"),
+    (168.75, 191.25, "S"),
+    (191.25, 213.75, "SSW"),
+    (213.75, 236.25, "SW"),
+    (236.25, 258.75, "WSW"),
+    (258.75, 281.25, "W"),
+    (281.25, 303.75, "WNW"),
+    (303.75, 326.25, "NW"),
+    (326.25, 348.75, "NNW")
+]
